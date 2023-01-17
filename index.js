1d280e5c196746264f9bd12f4891e174cbfa442c
NASA_Mark <76894588+Atharane@users.noreply.github.com>
2023-01-17 09:53:26 +0530

    add user join logic

--- index.js
+++ index.js
@@ -19,15 +19,13 @@ let users = [];
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
 
-  //Listens when a new user joins the server
   socket.on("newUser", (data) => {
     users.push(data);
-    console.log(users);
-    console.log(data);
     socketIO.emit("newUserResponse", users);
+    data.type = "join";
+    socketIO.emit("messageResponse", data);
   });
 
-  //Listen & log the message to the console
   socket.on("message", (data) => {
     console.log(data);
     socketIO.emit("messageResponse", data);
